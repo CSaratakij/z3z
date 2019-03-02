802bb237e16feb67992bd38447864f9cf5b01631
Chatchai Saratakij <CSaratakij@gmail.com>
2019-03-03 03:31:39 +0700

    Add : Init Project

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,32 @@
+﻿namespace Z3Z
+{
+    public class GameController
+    {
+        public delegate void _Func();
+
+        public static event _Func OnGameStart;
+        public static event _Func OnGameOver;
+
+        public static bool IsGameStart { get; private set; }
+
+
+        public static void GameStart()
+        {
+            if (IsGameStart)
+                return;
+
+            IsGameStart = true;
+            OnGameStart?.Invoke();
+        }
+
+        public static void GameOver()
+        {
+            if (!IsGameStart)
+                return;
+
+            IsGameStart = false;
+            OnGameOver?.Invoke();
+        }
+    }
+}
+
