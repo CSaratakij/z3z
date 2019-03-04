69d9f094ca2e8028f183e69baa48d77bc3825f66
Chatchai Saratakij <CSaratakij@gmail.com>
2019-03-05 04:01:53 +0700

    Add : First person camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -6,15 +6,33 @@ namespace Z3Z
 {
     public class CameraController : MonoBehaviour
     {
+        const float MAX_ANGLE_X = 80.0f;
+        const float MAX_ANGLE_Y = 360.0f;
+
+        [SerializeField]
+        CursorLockMode cursurLockState;
+
+        [SerializeField]
+        [Range(1.0f, 10.0f)]
+        float sensitivity;
+
         [SerializeField]
         Transform target;
 
         [SerializeField]
         Vector3 offset;
 
-        [SerializeField]
-        float damp;
 
+        Vector2 mouseAxis;
+        Vector3 rotationAxis;
+
+        void Awake()
+        {
+            //Todo
+            //Should move these to pause menu controller
+            Cursor.lockState = cursurLockState;
+            Cursor.visible = false;
+        }
 
         void Update()
         {
@@ -24,16 +42,49 @@ namespace Z3Z
         void LateUpdate()
         {
             FollowTargetHandler();
+            RotateHandler();
         }
 
         void InputHandler()
         {
+            mouseAxis.x = Input.GetAxisRaw("Mouse X");
+            mouseAxis.y = Input.GetAxisRaw("Mouse Y");
 
+            rotationAxis.x += (-mouseAxis.y * sensitivity);
+            rotationAxis.x = Mathf.Clamp(rotationAxis.x, -MAX_ANGLE_X, MAX_ANGLE_X);
+
+            rotationAxis.y += (mouseAxis.x * sensitivity);
+
+            if (rotationAxis.y > MAX_ANGLE_Y) {
+                rotationAxis.y = rotationAxis.y - MAX_ANGLE_Y;
+            }
+            else if (rotationAxis.y < -MAX_ANGLE_Y) {
+                rotationAxis.y = MAX_ANGLE_Y + rotationAxis.y;
+            }
         }
 
         void FollowTargetHandler()
         {
+            transform.position = target.position + offset;
+        }
 
+        void RotateHandler()
+        {
+            RotateCamera();
+            RotateTarget();
+        }
+
+        void RotateCamera()
+        {
+            Quaternion targetRotation = Quaternion.Euler(rotationAxis.x, rotationAxis.y, 0.0f);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, 0.6f);
+        }
+
+        void RotateTarget()
+        {
+            Vector3 lookVector = transform.forward;
+            lookVector.y = 0.0f;
+            target.localRotation = Quaternion.LookRotation(lookVector, Vector3.up);
         }
     }
 }
