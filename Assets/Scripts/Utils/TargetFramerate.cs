75fc29a96f2118e32188d2e10181b04d52477535
Chatchai Saratakij <CSaratakij@gmail.com>
2019-03-06 05:00:01 +0700

    Add : Target framerate at 60 fps

---
+++ Assets/Scripts/Utils/TargetFramerate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Z3Z
+{
+    public class TargetFramerate : MonoBehaviour
+    {
+        [SerializeField]
+        int targetFrameRate = 60;
+
+
+        void Awake()
+        {
+            Application.targetFrameRate = targetFrameRate;
+        }
+    }
+}
+
