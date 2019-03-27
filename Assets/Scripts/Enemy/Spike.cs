a8d1f4a6fb9e6881c78859a38a304c18918d3893
Chatchai Saratakij <CSaratakij@gmail.com>
2019-03-27 18:07:39 +0700

    Add : Spike

---
+++ Assets/Scripts/Enemy/Spike.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Z3Z
+{
+    public class Spike : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player")) {
+                GameController.Reset();
+                var scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.buildIndex);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Restarter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Z3Z
+{
+    public class Restarter : MonoBehaviour
+    {
+        const int UPDATE_RATE = 30;
+
+        [SerializeField]
+        Transform player;
+
+
+        void Update()
+        {
+            if (Time.frameCount % UPDATE_RATE == 0) {
+                RestartHandler();
+            }
+        }
+
+        void RestartHandler()
+        {
+            if (player.position.y < transform.position.y) {
+                GameController.Reset();
+                var scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.buildIndex);
+            }
+        }
+    }
+}
