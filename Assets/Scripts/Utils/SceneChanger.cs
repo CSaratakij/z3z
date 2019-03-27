c21b6935a0aa79660069f218b113b8d1c4ada313
Chatchai Saratakij <CSaratakij@gmail.com>
2019-03-27 21:50:37 +0700

    Add : Thank you for playing

---
+++ Assets/Scripts/Utils/SceneChanger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Z3Z
+{
+    [RequireComponent(typeof(Timer))]
+    public class SceneChanger : MonoBehaviour
+    {
+        Timer timer;
+
+        void Awake()
+        {
+            Initialize();
+            SubscribeEvent();
+        }
+
+        void Start()
+        {
+            timer.Countdown();
+        }
+
+        void OnDestroy()
+        {
+            UnsubscribeEvent();
+        }
+
+        void Initialize()
+        {
+            timer = GetComponent<Timer>();
+        }
+
+        void SubscribeEvent()
+        {
+            timer.OnStop += OnStop;
+        }
+
+        void UnsubscribeEvent()
+        {
+            timer.OnStop -= OnStop;
+        }
+
+        void OnStop()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
