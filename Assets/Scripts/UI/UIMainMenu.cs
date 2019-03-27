2d4b5fc035ad3ceebadf6b221757262fa46687e7
Chatchai Saratakij <CSaratakij@gmail.com>
2019-03-27 22:13:09 +0700

    Add : mainmenu

---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Z3Z
+{
+    public class UIMainMenu : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
